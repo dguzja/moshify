9f008b8d1dc2321881938961162fd2fa5e9b25e3
dioguz07 <dionguzja@gmail.com>
2025-05-11 02:59:33 +0200

    Build the navbar component integrated in the index.html page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-const collapsibles = document.querySelectorAll('.collapsible');
-collapsibles.forEach(item => {
-    item.querySelector('.collapsible__header').addEventListener('click', () => {
-        item.classList.toggle('collapsible--expanded');
-    });
+const collapsibles = document.querySelectorAll(".collapsible");
+
+collapsibles.forEach((item) => {
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  });
 });
