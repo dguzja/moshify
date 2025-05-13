8754f03ed71952c99aff1d6c3fd1225aba544a5d
dioguz07 <dionguzja@gmail.com>
2025-05-13 20:10:34 +0200

    Build the final touches

--- js/main.js
+++ js/main.js
@@ -3,5 +3,5 @@ const collapsibles = document.querySelectorAll(".collapsible");
 collapsibles.forEach((item) => {
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
-  });
+    });
 });
